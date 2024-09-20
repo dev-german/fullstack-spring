65b5c2672c62b71dc95e6f38824c972b7b27c4a4
German Granados <dev.germane@gmail.com>
2024-09-20 14:19:21 -0500

    feat: Create customer/show toast

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createStandaloneToast } from "@chakra-ui/react";
+
+const { ToastContainer } = createStandaloneToast();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider>
       <App />
+      <ToastContainer />      
     </ChakraProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- frontend/react/src/services/client.js
+++ frontend/react/src/services/client.js
@@ -1,9 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const getCustomers = async () => {
-    try {
-        return await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`)
-    } catch (err) {
-        throw err;
-    }
-}
+  try {
+    return await axios.get(
+      `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`
+    );
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const saveCustomer = async (customer) => {
+  try {
+    return await axios.post(
+      `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`,
+      customer
+    );
+  } catch (err) {
+    throw err;
+  }
+};
